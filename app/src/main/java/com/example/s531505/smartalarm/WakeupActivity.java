04c85851d3151188424c40ac14903cc21d02d904
dattubhargav <dattu.bhargav@git.com>
2018-10-05 18:36:29 -0500

    wake up activity is added

--- app/src/main/java/com/example/s531505/smartalarm/WakeupActivity.java
+++ app/src/main/java/com/example/s531505/smartalarm/WakeupActivity.java
@@ -1,7 +1,9 @@
 package com.example.s531505.smartalarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WakeupActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class WakeupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wakeup);
     }
+public void save(View v){
+      Intent intent=new Intent(this,StartActivity.class);
+      startActivity(intent);
+
+}
+    public void cancel(View v){
+        Intent intent1=new Intent(this,StartActivity.class);
+        startActivity(intent1);
+    }
+
 }
