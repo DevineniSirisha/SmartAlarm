42d5aecfb64acb6b284b63f59ca35d663d597ad2
manogna sivangula <manu.sivangula@gmail.com>
2018-10-25 23:05:56 -0500

    Action bar created commit for milestone 02

--- app/src/main/java/com/example/s531505/smartalarm/StartActivity.java
+++ app/src/main/java/com/example/s531505/smartalarm/StartActivity.java
@@ -3,6 +3,8 @@ package com.example.s531505.smartalarm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,6 +15,13 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the
+        //action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
     public void wakeup(View v){
         Intent intent=new Intent(this,WakeupActivity.class);
@@ -29,5 +38,28 @@ public class StartActivity extends AppCompatActivity {
         startActivityForResult(intent,1);
         Toast.makeText(StartActivity.this, "You are in Location Activity!",
                 Toast.LENGTH_LONG).show();
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.wake_up:
+                Intent intent=new Intent(this,WakeupActivity.class);
+                String s="Set Wake-up alarm";
+                intent.putExtra("msg",s);
+                startActivityForResult(intent,1);
+                Toast.makeText(StartActivity.this, "You are in Wake-up Activity!",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.location:
+                Intent init=new Intent(this,LocationActivity.class);
+                String st="Set location alarm";
+                init.putExtra("msg",st);
+                startActivityForResult(init,1);
+                Toast.makeText(StartActivity.this, "You are in Location Activity!",
+                        Toast.LENGTH_LONG).show();
+                return true;
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
