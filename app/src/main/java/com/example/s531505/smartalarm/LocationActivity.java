a3accfb020f2b31dd12de68c394aae8abb95df47
Devineni,Sai Sirisha <S531367@nwmissouri.edu>
2018-10-26 20:55:13 -0500

    Added a java file to get the latitude and longitude of the current location

--- app/src/main/java/com/example/s531505/smartalarm/LocationActivity.java
+++ app/src/main/java/com/example/s531505/smartalarm/LocationActivity.java
@@ -1,11 +1,15 @@
 package com.example.s531505.smartalarm;
 
+import android.Manifest;
 import android.content.Intent;
+import android.location.Location;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -13,6 +17,19 @@ public class LocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        ActivityCompat.requestPermissions(LocationActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+    }
+
+    public void onClick(View v) {
+        LatAndLong gt = new LatAndLong(getApplicationContext());
+        Location l = gt.getLocationOfUser();
+        if( l == null){
+            Toast.makeText(getApplicationContext(),"GPS unable to get Value",Toast.LENGTH_SHORT).show();
+        }else {
+            double lat = l.getLatitude();
+            double lon = l.getLongitude();
+            Toast.makeText(getApplicationContext(),"GPS Lat = "+lat+"\n lon = "+lon,Toast.LENGTH_SHORT).show();
+        }
     }
     public void Savebtn(View v){
         Intent intent=new Intent(this,StartActivity.class);
