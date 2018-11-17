074a58934f91d9d17affd162c7c36e4d0e2530c3
dattubhargav <dattu.bhargav@git.com>
2018-11-16 23:22:24 -0600

    Created Background Service

---
+++ app/src/main/java/com/example/s531505/smartalarm/CreateBackgroundServiceActivity.java
@@ -0,0 +1,13 @@
+package com.example.s531505.smartalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateBackgroundServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_background_service);
+    }
+}
